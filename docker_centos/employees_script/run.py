8d809237404e55635e4aef2f9f5a64c36a82fc4c
Isay Castaneda <isay.castaneda@bowheadhealth.com>
2022-08-01 19:40:09 -0500

    fix bug in centos populate db

--- docker_centos/employees_script/run.py
+++ docker_centos/employees_script/run.py
@@ -2,6 +2,12 @@ import os
 from app import script_csv
 
 if __name__ == "__main__":
-    print(os.getcwd())
-    script_csv.createTables()
-    script_csv.populate_table()
+
+    flag = True
+    while flag:
+        try:        
+            script_csv.createTables()
+            script_csv.populate_table()
+            flag=False
+        except :
+            print("db not redy yet")
